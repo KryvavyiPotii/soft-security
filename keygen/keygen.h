dd934b97203fd887455d82f1565db1b3aa80ab42
KryvavyiPotii <144026226+KryvavyiPotii@users.noreply.github.com>
2024-04-14 20:35:26 +0200

    Finished
    - Renamed several functions.
    - Improved function documentation with extra comments.

--- keygen/keygen.h
+++ keygen/keygen.h
@@ -1,6 +1,7 @@
 #include <windows.h>
 #include <sstream>
 
+// Window class.
 #define KEYGEN_CLASS L"Key generator class"
 
 // Offsets for adding window elements.
@@ -19,13 +20,42 @@
 #define MENU_ABOUT      30
 
 // GUI functions.
+// Window procedure.
 LRESULT CALLBACK KeygenProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+// Add controls to a window (text fields and buttons).
+// Arguments:
+//     [in] hwnd - a handle to the KeyGen window.
+// Return value:
+//     Success - 0.
+//     Failure - -1.
 int addControls(HWND hwnd);
+// Add menu bar to a window.
+// Arguments:
+//     [in] hwnd - a handle to the KeyGen window.
+// Return value:
+//     Success - 0.
+//     Failure - -1.
 int addMenu(HWND hwnd);
 
 // Password generation functions.
+// Generate a password (key) based on the entered username.
+// Arguments:
+//     [in] hwnd - a handle to the KeyGen window.
+// Return value:
+//     Success - 0.
+//     Failure - -1.
 int keygen(HWND hwnd);
+// Derive a password (key) number from a username.
+// Arguments:
+//     [in] pbUsername - a pointer to a BYTE array with a username.
+//     [in, out] puNumber - a pointer to a variable that receives the derived number.
+//     [in] iUsernameSize - size of a BYTE array pointed by pbUsername without null terminator.
 void deriveNumber(BYTE* pbUsername, UINT* puNumber, int iUsernameSize);
+// Create a password based on the derived number.
+// Arguments:
+//     [in] uNumber - the derived number.
+//     [in, out] pbPassword - a pointer to BYTE array that receives the created password.
+//         Size of the array must be 9 (8 BYTEs with null terminator).
 void createPassword(UINT uNumber, BYTE* pbPassword);
 
 // Debug functions.
