fb03108e0a93739221313bbb398294ebf591e1b0
KryvavyiPotii <144026226+KryvavyiPotii@users.noreply.github.com>
2024-04-14 10:52:32 +0200

    Update keygen.cpp
    All functionality and GUI features were implemented.
    Tests needed

--- keygen/keygen.cpp
+++ keygen/keygen.cpp
@@ -1,5 +1,7 @@
 #include <keygen.h>
 
+const wchar_t wcAboutProgram[] = L"A program to generate a password for \"Crack_me_up!.exe\" based on the entered username.";
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
     // Class creation and registration.
@@ -65,7 +67,25 @@ LRESULT CALLBACK KeygenProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     case WM_COMMAND:
         switch (LOWORD(wParam))
         {
-        
+        case CID_GENERATE:
+            if (keygen(hwnd) < 0)
+            {
+                showError(L"KeygenProc::keygen");
+            }
+            break;
+
+        case CID_CANCEL:
+            DestroyWindow(hwnd);
+            break;
+
+        case MENU_ABOUT:
+            MessageBox(
+                hwnd,
+                wcAboutProgram,
+                L"About program",
+                MB_OK | MB_ICONINFORMATION
+            );
+            break;
         }
 
         return 0;
@@ -109,7 +129,7 @@ int addControls(HWND hwnd)
         L"STATIC", L"Password:",
         WS_CHILD | WS_VISIBLE,
         ELEMENT_OFFSET,
-        ELEMENT_OFFSET * 3,
+        ELEMENT_OFFSET * 4,
         (KEYGEN_WIDTH) / 3,
         ELEMENT_HEIGHT,
         hwnd, NULL, NULL, NULL
@@ -121,9 +141,9 @@ int addControls(HWND hwnd)
 
     if (!CreateWindow(
         L"EDIT", L"",
-        ES_READONLY | WS_BORDER | WS_CHILD | WS_VISIBLE,
+        ES_READONLY | WS_CHILD | WS_VISIBLE,
         ELEMENT_OFFSET + (KEYGEN_WIDTH) / 3,
-        ELEMENT_OFFSET * 3,
+        ELEMENT_OFFSET * 4,
         (KEYGEN_WIDTH - ELEMENT_OFFSET) / 2,
         ELEMENT_HEIGHT,
         hwnd, (HMENU)CID_PASSWORD, NULL, NULL
@@ -199,48 +219,144 @@ int addMenu(HWND hwnd)
     return 0;
 }
 
-void CreateNum(BYTE* pbUsername, UINT* pNum, int iUsernameSize)
+int keygen(HWND hwnd)
+{
+    // Get username.
+    BYTE* pbUsername;
+    DWORD dwLength;
+
+    {
+        // Get handle to username field.
+        HWND hwndUsername = GetDlgItem(hwnd, CID_USERNAME);
+
+        if (hwndUsername == NULL)
+        {
+            showError(L"keygen::GetDlgItem::USERNAME");
+            return -1;
+        }
+
+        // Get username length.
+        dwLength = GetWindowTextLengthA(hwndUsername);
+
+        if (!dwLength)
+        {
+            if (!GetLastError())
+            {
+                MessageBox(
+                    NULL,
+                    L"Username is empty.",
+                    L"Alert",
+                    MB_OK | MB_ICONWARNING
+                );
+            }
+            else
+            {
+                showError(L"keygen::GetWindowTextLength::USERNAME");
+            }
+            return -1;
+        }
+
+        // Resize buffer.
+        pbUsername = new BYTE[dwLength + 2];
+        pbUsername[dwLength + 1] = 0;
+
+        int iSize = GetWindowTextA(hwndUsername, (char*)pbUsername, dwLength + 1);
+
+        if (!iSize && GetLastError())
+        {
+            showError(L"identify::GetWindowText::USERNAME");
+            return -1;
+        }
+    }
+
+    // DEBUG
+    // MessageBoxA(NULL, (char*)pbUsername, "Username", 0);
+
+    UINT uNumber;
+
+    deriveNumber(pbUsername, &uNumber, dwLength + 1);
+
+    // Cleanup.
+    delete[] pbUsername;
+
+    // DEBUG
+    //showValue(L"uNumber", uNumber);
+
+    BYTE* pbPassword = new BYTE[9];
+
+    createPassword(uNumber, pbPassword);
+
+    // Add null byte.
+    pbPassword[8] = 0;
+
+    // DEBUG
+    // MessageBoxA(NULL, (char*)pbPassword, "Password", 0);
+
+    // Show output.
+    {
+        // Get handle to password field.
+        HWND hwndPassword = GetDlgItem(hwnd, CID_PASSWORD);
+
+        if (hwndPassword == NULL)
+        {
+            showError(L"keygen::GetDlgItem::PASSWORD");
+            return -1;
+        }
+
+        if (!SetWindowTextA(hwndPassword, (char*)pbPassword))
+        {
+            showError(L"keygen::SetWindowTextA::PASSWORD");
+            return -1;
+        }
+    }
+
+    // Cleanup.
+    delete[] pbPassword;
+
+    return 0;
+}
+
+void deriveNumber(BYTE* pbUsername, UINT* puNumber, int iUsernameSize)
 
 {
     int iCounter = 0;
 
-    *pNum = 0xfacc0fff;
+    *puNumber = 0xfacc0fff;
 
     if (0 < iUsernameSize)
     {
         do
         {
-            *pNum = (pbUsername[iCounter] ^ *pNum) << 8 | *pNum >> 0x18;
+            *puNumber = (pbUsername[iCounter] ^ *puNumber) << 8 | *puNumber >> 0x18;
             iCounter++;
         } while (iCounter < iUsernameSize);
     }
-
-    return;
 }
 
-void CreatePassword(UINT uNum, BYTE* pbPassword)
-
+void createPassword(UINT uNumber, BYTE* pbPassword)
 {
-    UINT uVal;
     int iCounter = 0;
     BYTE bCharacter = 0;
 
     do
     {
-        uVal = uNum & 0xf;
-        uNum >>= 4;
+        // Get a nibble.
+        UINT uNibble = uNumber & 0xf;
 
-        if (9 < uVal)
+        // Go to the next nibble.
+        uNumber >>= 4;
+
+        // Allow only numbers between 0 and 9 inclusively.
+        if (uNibble > 9)
         {
-            uVal = 9;
+            uNibble = 9;
         }
 
-        _itoa_s(uVal, (char*)&bCharacter, 1, 10);
-        pbPassword[iCounter] = bCharacter;
+        // Convert and add the digit to password.
+        pbPassword[iCounter] = uNibble + 0x30;
+
         iCounter++;
     } while (iCounter < 8);
-
-    return;
 }
 
 void showError(const std::wstring& wstrError)
@@ -253,4 +369,15 @@ void showError(const std::wstring& wstrError)
     std::wstring wstr = wsstr.str();
 
     MessageBox(NULL, wstr.c_str(), L"Error", MB_OK);
+}
+
+void showValue(const std::wstring& wstrName, auto aValue)
+{
+    std::wstringstream wssValue;
+
+    wssValue << aValue << L" (0x" << std::hex << aValue << L")" << std::endl;
+
+    std::wstring wsValue = wssValue.str();
+
+    MessageBox(NULL, wsValue.c_str(), wstrName.c_str(), 0);
 }
